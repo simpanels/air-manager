cd1dbb56b75afb5ce54d7838ab82c5eff0e92621
Charlie Drage <charlie@charliedrage.com>
2024-12-04 15:08:43 -0500

    update 6
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- readme.py
+++ readme.py
@@ -8,9 +8,9 @@ def generate_readme(directory):
         "We are a **fork** of [Simstrumentation](https://github.com/Simstrumentation/Air-Manager) as we update and add new instruments for both Microsoft Flight Simulator 2020 as well as 2024.\n\n"
         "We are active on their [Discord](https://discord.gg/6xkCXe5pMn) if you have any questions!\n\n"
         "## Submit your own instruments\n\n"
-        "[Click on our Google Form](https://forms.gle/6TeXh5Q3rHtmLRB87) to submit your own instruments. We would be VERY happy to see your creations! Our script will automatically create a pull request and update the source code so any changes are seen publically.\n\n"
+        "[Click on our Google Form](https://forms.gle/6TeXh5Q3rHtmLRB87) to submit your own instruments.\n\nWe would be VERY happy to see your creations! Our script will automatically create a pull request and update the source code so any changes are seen publically.\n\n"
         "## Important Notes\n\n"
-        "• Due to numerous issues with MSFS2024, we have all files in a separate folder. We will NOT provide panels which are compatible with both versions from one siff file.\n"
+        "Due to numerous issues with MSFS2024, we have all files in a separate folder. We will NOT provide panels which are compatible with both versions from one siff file. You must use the separate siff.\n"
     )
     footer = "\n\n---\n\nAutomatically generated with ❤️ (and Python)."
     msfs_section_header = "## Microsoft Flight Simulator 2020\n\nThis section lists the aircraft and their available instruments.\n\n"
@@ -38,7 +38,9 @@ def generate_readme(directory):
                 if file.endswith(".siff"):
                     instrument_name = file.rsplit(".", 1)[0].replace("_", " ").title()
                     instrument_path = os.path.relpath(os.path.join(root, file), directory)
-                    instruments.append(f"• [{instrument_name}](./{instrument_path})")
+                    raw_instrument_path = instrument_path.replace("blob/", "")
+                    raw_instrument_link = f"https://raw.githubusercontent.com/simpanels/airmanager/main/{raw_instrument_path}"
+                    instruments.append(f"• [{instrument_name}]({raw_instrument_link})")
         
         instruments_list = "<br>".join(instruments) if instruments else "No Instruments"
         table_rows.append(f"| {name} | {image_link} | {instruments_list} |")
